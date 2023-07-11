8f881176012db97dfa69b24b4b2083d45582f93c
LUCIEFR <ani.mimansatech@gmail.com>
2023-07-11 22:47:42 +0530

    upp

--- src/api.js
+++ src/api.js
@@ -345,8 +345,7 @@ const ScoreController = {
 	// sportskeeda
 	sportskeeda: async (req, res) => {
 		var score = {};
-		var match_urls = [];
-		
+
 		var requestOptions = {
 			"credentials": "omit",
 			"headers": {
@@ -368,225 +367,207 @@ const ScoreController = {
 				"https://push.sportskeeda.com/get-cricket-matches/featured",
 				requestOptions
 			);
+
 			const response_data = await response.json();
-			response_data.matches.forEach((data, key) => {
+			await Promise.allSettled(response_data.matches.map(async (data, key) => {
 				var converted = data.link.replace(
 					"www.sportskeeda.com",
 					"cmc.sportskeeda.com"
 				);
-				match_urls.push(converted + "/ajax?lang=en");
-			});
 
-			async function sk(){
-				match_urls.forEach(async (url, key) => {
-					// console.log("match-->", match_urls); 
-					const match_response = await fetch(url);
-					const sc = await match_response.json();
-					if (sc.commentary && sc.commentary.length > 0) {
-						// console.log('sc.commentary-->', sc.commentary)
+				converted = `${converted}/ajax?lang=en`
+				const match_response = await fetch(converted);
+				const sc = await match_response.json();
+				if (sc.commentary && sc.commentary.length > 0) {
+					var last_six_balls = [];
+					var ball_run = '---'
 
-						var last_six_balls = [];
-						var ball_run = '---'
+					for (let scr of sc.commentary) {
+						if (scr.opta_ball_type != undefined && scr.opta_ball_type != 'end of over') {
 
-						for (let scr of sc.commentary) {
-							if (scr.opta_ball_type != undefined && scr.opta_ball_type != 'end of over') {
+							var scr_runs = (scr.runs != undefined ? scr.runs : '')
 
-								var scr_runs = (scr.runs != undefined ? scr.runs : '')
+							switch (scr.opta_ball_type) {
+								case 'normal':
+									ball_run = (scr.runs != undefined ? scr.runs : '0');
+									break;
 
-								switch (scr.opta_ball_type) {
-									case 'normal':
-										ball_run = (scr.runs != undefined ? scr.runs : '0');
-										break;
+								case 'one':
+									ball_run = "1";
+									break;
 
-									case 'one':
-										ball_run = "1";
-										break;
+								case 'two':
+									ball_run = "2";
+									break;
 
-									case 'two':
-										ball_run = "2";
-										break;
+								case 'three':
+									ball_run = "3";
+									break;
 
-									case 'three':
-										ball_run = "3";
-										break;
+								case 'four':
+									ball_run = "4";
+									break;
 
-									case 'four':
-										ball_run = "4";
-										break;
+								case 'five':
+									ball_run = "5";
+									break;
 
-									case 'five':
-										ball_run = "5";
-										break;
+								case 'six':
+									ball_run = "6";
+									break;
 
-									case 'six':
-										ball_run = "6";
-										break;
+								case 'wicket':
+								case 'catch':
+								case 'run out':
+									ball_run = scr_runs + "W";
+									break;
 
-									case 'wicket':
-									case 'catch':
-									case 'run out':
-										ball_run = scr_runs + "W";
-										break;
+								case 'wide':
+									ball_run = scr_runs + "wd";
+									break;
 
-									case 'wide':
-										ball_run = scr_runs + "wd";
-										break;
+								case 'no_ball':
+									ball_run = scr_runs + "nb";
+									break;
 
-									case 'no_ball':
-										ball_run = scr_runs + "nb";
-										break;
+								case 'leg_bye':
+									ball_run = scr_runs + "lb";
+									break;
 
-									case 'leg_bye':
-										ball_run = scr_runs + "lb";
-										break;
+								case 'bye':
+									ball_run = scr_runs + "b";
+									break;
+							}
 
-									case 'bye':
-										ball_run = scr_runs + "b";
-										break;
-								}
-
-								if (last_six_balls.length < 7) {
-									last_six_balls.push(ball_run)
-								}
+							if (last_six_balls.length < 7) {
+								last_six_balls.push(ball_run)
 							}
 						}
 					}
+				}
 
-					var score_obj = {
-						match_urls: `https://www.sportskeeda.com/live-cricket-score/${sc.topic_slug}`,
+				var score_obj = {
+					match_urls: `https://www.sportskeeda.com/live-cricket-score/${sc.topic_slug}`,
 
-						match_api_url: `https://cmc.sportskeeda.com/live-cricket-score/${sc.topic_slug}/ajax?lang=en`,
+					match_api_url: `https://cmc.sportskeeda.com/live-cricket-score/${sc.topic_slug}/ajax?lang=en`,
 
-						match_status: (sc.match_status ? sc.match_status : ''),
+					match_status: (sc.match_status ? sc.match_status : ''),
 
-						t1: {
-							f: sc.score_strip[0].name,
-							n: sc.score_strip[0].short_name,
-						},
+					t1: {
+						f: sc.score_strip[0].name,
+						n: sc.score_strip[0].short_name,
+					},
 
-						t2: {
-							f: sc.score_strip[1].name,
-							n: sc.score_strip[1].short_name,
-						},
-						i1: {
-							sc:
-								sc.score_strip && sc.score_strip[0].score != ""
-									? sc.score_strip[0].score.split("(")[0].split("/")[0].trim()
-									: "0",
-							wk:
-								sc.score_strip && sc.score_strip[0].score != ""
-									? sc.score_strip[0].score.split("(")[0].split("/")[1].trim()
-									: "0",
-							ov:
-								sc.score_strip && sc.score_strip[0].score != ""
-									? sc.score_strip[0].score.split("(")[1].split(" ")[0].trim()
-									: "0",
-						},
+					t2: {
+						f: sc.score_strip[1].name,
+						n: sc.score_strip[1].short_name,
+					},
+					i1: {
+						sc:
+							sc.score_strip && sc.score_strip[0].score != ""
+								? sc.score_strip[0].score.split("(")[0].split("/")[0].trim()
+								: "0",
+						wk:
+							sc.score_strip && sc.score_strip[0].score != ""
+								? sc.score_strip[0].score.split("(")[0].split("/")[1].trim()
+								: "0",
+						ov:
+							sc.score_strip && sc.score_strip[0].score != ""
+								? sc.score_strip[0].score.split("(")[1].split(" ")[0].trim()
+								: "0",
+					},
 
-						i2: {
-							sc:
-								sc.score_strip && sc.score_strip[1].score != ""
-									? sc.score_strip[1].score.split("(")[0].split("/")[0].trim()
-									: "0",
-							wk:
-								sc.score_strip && sc.score_strip[1].score != ""
-									? sc.score_strip[1].score.split("(")[0].split("/")[1].trim()
-									: "0",
-							ov:
-								sc.score_strip && sc.score_strip[1].score != ""
-									? sc.score_strip[1].score.split("(")[1].split(" ")[0].trim()
-									: "0",
-						},
+					i2: {
+						sc:
+							sc.score_strip && sc.score_strip[1].score != ""
+								? sc.score_strip[1].score.split("(")[0].split("/")[0].trim()
+								: "0",
+						wk:
+							sc.score_strip && sc.score_strip[1].score != ""
+								? sc.score_strip[1].score.split("(")[0].split("/")[1].trim()
+								: "0",
+						ov:
+							sc.score_strip && sc.score_strip[1].score != ""
+								? sc.score_strip[1].score.split("(")[1].split(" ")[0].trim()
+								: "0",
+					},
 
-						i3: {
-							sc: "0",
-							wk: "0",
-							ov: "0",
-						},
+					i3: {
+						sc: "0",
+						wk: "0",
+						ov: "0",
+					},
 
-						i4: {
-							sc: "0",
-							wk: "0",
-							ov: "0",
-						},
+					i4: {
+						sc: "0",
+						wk: "0",
+						ov: "0",
+					},
 
-						cs: {
-							msg: sc.secondary_info ? sc.secondary_info : "",
-						},
+					cs: {
+						msg: sc.secondary_info ? sc.secondary_info : "",
+					},
 
-						iov: "",
+					iov: "",
 
-						p1:
-							sc.now_batting && sc.now_batting.b1 && sc.now_batting.b1.name
-								? sc.now_batting.b1.name
-								: "",
+					p1:
+						sc.now_batting && sc.now_batting.b1 && sc.now_batting.b1.name
+							? sc.now_batting.b1.name
+							: "",
 
-						p2:
-							sc.now_batting && sc.now_batting.b2 && sc.now_batting.b2.name
-								? sc.now_batting.b2.name
-								: "",
+					p2:
+						sc.now_batting && sc.now_batting.b2 && sc.now_batting.b2.name
+							? sc.now_batting.b2.name
+							: "",
 
-						os: "p1",
+					os: "p1",
 
-						b1s: `${sc.now_batting && sc.now_batting.b1 && sc.now_batting.b1.stats
-								? (sc.now_batting.b1.stats.runs === '' ? '0' : sc.now_batting.b1.stats.runs)
-								: "0"
-							},${sc.now_batting && sc.now_batting.b1 && sc.now_batting.b1.stats
-								? (sc.now_batting.b1.stats.balls === '' ? '0' : sc.now_batting.b1.stats.balls)
-								: "0"
-							},${sc.now_batting && sc.now_batting.b1 && sc.now_batting.b1.stats
-								? (sc.now_batting.b1.stats.fours === '' ? '0' : sc.now_batting.b1.stats.fours)
-								: "0"
-							},${sc.now_batting && sc.now_batting.b1 && sc.now_batting.b1.stats
-								? (sc.now_batting.b1.stats.sixes === '' ? '0' : sc.now_batting.b1.stats.sixes)
-								: "0"
-							}`,
+					b1s: `${sc.now_batting && sc.now_batting.b1 && sc.now_batting.b1.stats
+						? (sc.now_batting.b1.stats.runs === '' ? '0' : sc.now_batting.b1.stats.runs)
+						: "0"
+						},${sc.now_batting && sc.now_batting.b1 && sc.now_batting.b1.stats
+							? (sc.now_batting.b1.stats.balls === '' ? '0' : sc.now_batting.b1.stats.balls)
+							: "0"
+						},${sc.now_batting && sc.now_batting.b1 && sc.now_batting.b1.stats
+							? (sc.now_batting.b1.stats.fours === '' ? '0' : sc.now_batting.b1.stats.fours)
+							: "0"
+						},${sc.now_batting && sc.now_batting.b1 && sc.now_batting.b1.stats
+							? (sc.now_batting.b1.stats.sixes === '' ? '0' : sc.now_batting.b1.stats.sixes)
+							: "0"
+						}`,
 
-						b2s: `${sc.now_batting && sc.now_batting.b2 && sc.now_batting.b2.stats != ''
-								? sc.now_batting.b2.stats.runs
-								: "0"
-							},${sc.now_batting && sc.now_batting.b2 && sc.now_batting.b2.stats
-								? sc.now_batting.b2.stats.balls
-								: "0"
-							},${sc.now_batting && sc.now_batting.b2 && sc.now_batting.b2.stats
-								? sc.now_batting.b2.stats.fours
-								: "0"
-							},${sc.now_batting && sc.now_batting.b2 && sc.now_batting.b2.stats
-								? sc.now_batting.b2.stats.sixes
-								: "0"
-							}`,
+					b2s: `${sc.now_batting && sc.now_batting.b2 && sc.now_batting.b2.stats != ''
+						? sc.now_batting.b2.stats.runs
+						: "0"
+						},${sc.now_batting && sc.now_batting.b2 && sc.now_batting.b2.stats
+							? sc.now_batting.b2.stats.balls
+							: "0"
+						},${sc.now_batting && sc.now_batting.b2 && sc.now_batting.b2.stats
+							? sc.now_batting.b2.stats.fours
+							: "0"
+						},${sc.now_batting && sc.now_batting.b2 && sc.now_batting.b2.stats
+							? sc.now_batting.b2.stats.sixes
+							: "0"
+						}`,
 
-						bw: sc.now_bowling ? sc.now_bowling.b1.name : "",
+					bw: sc.now_bowling ? sc.now_bowling.b1.name : "",
 
 
-						pb: (last_six_balls && last_six_balls.length > 0 ?
-							`${(last_six_balls.length > 5 ? last_six_balls[0] : '')
-							},${(last_six_balls.length > 4 ? last_six_balls[1] : '')
-							},${(last_six_balls.length > 3 ? last_six_balls[2] : '')
-							},${(last_six_balls.length > 2 ? last_six_balls[3] : '')
-							},${(last_six_balls.length > 1 ? last_six_balls[4] : '')
-							},${(last_six_balls.length > 0 ? last_six_balls[5] : '')
-							}` : ''),
-					};
+					pb: (last_six_balls && last_six_balls.length > 0 ?
+						`${(last_six_balls.length > 5 ? last_six_balls[0] : '')
+						},${(last_six_balls.length > 4 ? last_six_balls[1] : '')
+						},${(last_six_balls.length > 3 ? last_six_balls[2] : '')
+						},${(last_six_balls.length > 2 ? last_six_balls[3] : '')
+						},${(last_six_balls.length > 1 ? last_six_balls[4] : '')
+						},${(last_six_balls.length > 0 ? last_six_balls[5] : '')
+						}` : ''),
+				};
 
-					score[sc.match_id] = score_obj;
+				score[sc.match_id] = score_obj;
 
-					// console.log(`Score: ${JSON.stringify(score_obj)}`);
-
-				});
-
-				// console.log(`Score---> ${JSON.stringify(score)}`);	
-
-				console.log(Object.keys(score).length);
-				if (Object.keys(score).length>0) {
-					res.status(200).json(score);;
-				}		
-				// if (score) {
-				// 	res.status(200).json(score);
-				// }
-			}sk()
+			}));
 			
-			
+			res.status(200).json(score);;
 
 		} catch (error) {
 			console.log('error -->', error);
